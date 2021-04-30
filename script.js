ff6e3a9c85f1cfd927687b22e83df12d4af86c2c
SimoneSaramondi <simo98sara@gmail.com>
2021-04-30 14:49:07 +0200

    inizialising

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const app = new Vue ({
     el: "#roof",
     data:{
-        
+        myImg:[
+            {
+                img: 'https://www.corriere.it/methode_image/2019/04/14/Sport/Foto%20Sport%20-%20Trattate/cairoli-klaC-U31101298247247wmG-656x492@Corriere-Web-Sezioni.PNG'
+            },
+            {
+                img: 'https://www.corriere.it/methode_image/2019/04/14/Sport/Foto%20Sport%20-%20Trattate/cairoli-klaC-U31101298247247wmG-656x492@Corriere-Web-Sezioni.PNG'
+            },
+            {
+                img: 'https://www.corriere.it/methode_image/2019/04/14/Sport/Foto%20Sport%20-%20Trattate/cairoli-klaC-U31101298247247wmG-656x492@Corriere-Web-Sezioni.PNG'
+            },
+        ]
     }
 })
