e809e2b3e4a4b882722a05ccdb367e29fa435486
SimoneSaramondi <simo98sara@gmail.com>
2021-05-02 10:26:22 +0200

    inizio html, css, js

--- script.js
+++ script.js
@@ -2,13 +2,18 @@
 var app = new Vue({
     el: '#app',
     data: {
-        indice: 0,
-        
+
+        indiceMax: 4,
+
+        indice: 0,       
+
         immagini:[
             "https://static.sky.it/images_static/sport/motori/2019/03/02/ktm_factory_mxgp_cairoli_twitter.jpg.transform/hero-mobile-2x/ab015c242aaa1f94bb51cf6631e5d540925c7589/img.jpeg",
             "https://www.corriere.it/methode_image/2019/04/14/Sport/Foto%20Sport%20-%20Trattate/cairoli-klaC-U31101298247247wmG-656x492@Corriere-Web-Sezioni.PNG",
             "https://www.repstatic.it/content/nazionale/img/2020/12/06/184816776-2a24aca9-2ba5-4c41-99a2-5805dfaf223a.jpg"
         ],
-      
+        
     },
+
+
   })
