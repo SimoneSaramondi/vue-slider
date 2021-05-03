4203ffbda90ffa1f11ab1c8401637d4dfcc7385c
SimoneSaramondi <simo98sara@gmail.com>
2021-05-03 15:11:05 +0200

    finish project

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
 var app = new Vue({
     el: '#app',
-    data: {        
+    data: {     
+        
+        interval: null,
 
         indice: 0,       
 
@@ -9,20 +11,24 @@ var app = new Vue({
             "https://static.sky.it/images_static/sport/motori/2019/03/02/ktm_factory_mxgp_cairoli_twitter.jpg.transform/hero-mobile-2x/ab015c242aaa1f94bb51cf6631e5d540925c7589/img.jpeg",
             "https://www.corriere.it/methode_image/2019/04/14/Sport/Foto%20Sport%20-%20Trattate/cairoli-klaC-U31101298247247wmG-656x492@Corriere-Web-Sezioni.PNG",
             "https://www.repstatic.it/content/nazionale/img/2020/12/06/184816776-2a24aca9-2ba5-4c41-99a2-5805dfaf223a.jpg"
-        ],
+        ],    
 
-        
-        
-        
     },
 
     methods:{
         //click sulle frecce
         //+1 per avanti
         //-1 per indietro
-        changeImg(direction){
-            this.indice += direction;
-            //Modificare per il cambio con frecce
+        changeImg(direction, changebyAI){
+            let newIndex = this.indice += direction;
+
+            if(newIndex < 0){
+                newIndex = this.immagini.length - 1;
+            } else if (newIndex > (this.immagini.length - 1)) {                
+                newIndex = 0;
+            }
+
+            this.indice = newIndex;
         },
 
         //Uso la stessa funzione per cambiare immagini con la tastiera
@@ -31,18 +37,25 @@ var app = new Vue({
         onDotClick(clickIndex){
             this.indice = clickIndex;
         },
+        play(){
+            clearInterval(this.interval);
+
+            this.interval = setInterval(() => {
+                this.changeImg(+1, true);
+            }, 3500);
+        },
+        pause(){
+            clearInterval(this.interval)
+        }
     },
 
     //specifico a vue cosa deve fare quanto arriva mounted
-    mounted:function(){
+    mounted(){
         //Faccio focus su la classe container
         //quindi come se fosse gia in focus dal ricaricamento della pagina
         document.querySelector(".container").focus();
 
-        setInterval(() => {
-            this.changeImg(+1)
-        }, 5000)
+        this.play();
     }
-
 
   });
